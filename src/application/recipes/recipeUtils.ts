37cc6f8c6cc91b66630b9e4f0e579c47eb15ec1c
BenHunt17 <58478479+BenHunt17@users.noreply.github.com>
2024-01-29 23:00:44 +0000

    Scale ingredient values

--- src/application/recipes/recipeUtils.ts
+++ src/application/recipes/recipeUtils.ts
@@ -17,20 +17,32 @@ export async function getCollectiveNutrients(ingredients: Ingredient[]) {
   return foods.reduce<Nutrient[]>((acc, curr) => {
     curr.foodNutrients?.forEach((foodNutrient) => {
       if (
-        !(foodNutrient.name && foodNutrient.amount && foodNutrient.unitName)
+        !(
+          foodNutrient.number &&
+          foodNutrient.name &&
+          foodNutrient.amount &&
+          foodNutrient.unitName
+        )
       ) {
         return [];
       }
 
+      const quantity =
+        ingredients.find(
+          (ingredient) => ingredient.externalId === curr.fdcId.toString()
+        )?.quantity ?? DEFAULT_QUANTITY_G;
+      const amount = foodNutrient.amount * (quantity / DEFAULT_QUANTITY_G);
+
       const existingNutrientIdx = acc.findIndex(
-        (nutrient) => nutrient.name === foodNutrient.name
+        (nutrient) => nutrient.number === foodNutrient.number
       );
       if (acc[existingNutrientIdx]) {
         acc[existingNutrientIdx].value += foodNutrient.amount;
       } else {
         acc.push({
+          number: foodNutrient.number,
           name: foodNutrient.name,
-          value: foodNutrient.amount,
+          value: amount,
           unitName: foodNutrient.unitName,
         });
       }
@@ -38,3 +50,5 @@ export async function getCollectiveNutrients(ingredients: Ingredient[]) {
     return acc;
   }, []);
 }
+
+const DEFAULT_QUANTITY_G = 100;
--- src/domain/types/recipe/nutrient.ts
+++ src/domain/types/recipe/nutrient.ts
@@ -1,4 +1,5 @@
 export interface Nutrient {
+  number: number;
   name: string;
   value: number;
   unitName: string;
